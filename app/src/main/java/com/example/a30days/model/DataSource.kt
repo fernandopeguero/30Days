5753d3311f437ead09dfe39acc5f3f2813208ed4
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-11 22:32:25 -0500

    Add new day field to data list
    The data list now includes a day field to be able to display
    the day for each exercise correctly

--- app/src/main/java/com/example/a30days/model/DataSource.kt
+++ app/src/main/java/com/example/a30days/model/DataSource.kt
@@ -1,6 +1,5 @@
-package com.example.a30days.ui.theme
+package com.example.a30days.model
 
-import com.example.a30days.model.Exercise
 import com.example.a30days.R
 
 
@@ -8,155 +7,186 @@ object DataRepository {
 
     val exercises = listOf(
         Exercise(
+            day = 1,
             name = R.string.PushUps,
             imgUrl = R.drawable.day_1_push_ups,
             description = R.string.PushUps_description
         ),
         Exercise(
+            day = 2,
             name = R.string.Squats,
             imgUrl = R.drawable.day_2_squats,
             description = R.string.Squats_description
         ),
         Exercise(
+            day = 3,
             name = R.string.Plank,
             imgUrl = R.drawable.day_3_plank,
             description = R.string.Plank_description
         ),
         Exercise(
+            day = 4,
             name = R.string.Lunges,
             imgUrl = R.drawable.day_4_lunges,
             description = R.string.Lunges_description
         ),
         Exercise(
+            day = 5,
             name = R.string.MountainClimbers,
             imgUrl = R.drawable.day_05_mountain_climbers,
             description = R.string.MountainClimbers_description
         ),
         Exercise(
+            day = 6,
             name = R.string.Burpees,
             imgUrl = R.drawable.day_6_burpees,
             description = R.string.Burpees_description
         ),
         Exercise(
+            day = 7,
             name = R.string.JumpingJacks,
             imgUrl = R.drawable.day_7_jumping_jacks,
             description = R.string.JumpingJacks_description
         ),
         Exercise(
+            day = 8,
             name = R.string.SitUps,
             imgUrl = R.drawable.day_8_sit_ups,
             description = R.string.SitUps_description
         ),
         Exercise(
+            day = 9,
             name = R.string.BicycleCrunches,
             imgUrl = R.drawable.day_9_bicycle_crunches,
             description = R.string.BicycleCrunches_description
         ),
         Exercise(
+            day = 10,
             name = R.string.HighKnees,
             imgUrl = R.drawable.day_10_high_knees,
             description = R.string.HighKnees_description
         ),
         Exercise(
+            day = 11,
             name = R.string.TricepDips,
             imgUrl = R.drawable.day_11_triceps_dips,
             description = R.string.TricepDips_description
         ),
         Exercise(
+            day = 12,
             name = R.string.LegRaises,
             imgUrl = R.drawable.day_12_leg_raises,
             description = R.string.LegRaises_description
         ),
         Exercise(
+            day = 13,
             name = R.string.RussianTwists,
             imgUrl = R.drawable.day_13_russian_twists,
             description = R.string.RussianTwists_description
         ),
         Exercise(
+            day = 14,
             name = R.string.WallSit,
             imgUrl = R.drawable.day_14_wall_sit,
             description = R.string.WallSit_description
         ),
         Exercise(
+            day = 15,
             name = R.string.GluteBridge,
             imgUrl = R.drawable.day_15_glute_bridge,
             description = R.string.GluteBridge_description
         ),
         Exercise(
+            day = 16,
             name = R.string.SidePlank,
             imgUrl = R.drawable.day_16_side_plank,
             description = R.string.SidePlank_description
         ),
         Exercise(
+            day = 17,
             name = R.string.CalfRaises,
             imgUrl = R.drawable.day_17_calf_raises,
             description = R.string.CalfRaises_description
         ),
         Exercise(
+            day = 18,
             name = R.string.ArmCircles,
             imgUrl = R.drawable.day_18_arm_circles,
             description = R.string.ArmCircles_description
         ),
         Exercise(
+            day = 19,
             name = R.string.Superman,
             imgUrl = R.drawable.day_19_superman,
             description = R.string.Superman_description
         ),
         Exercise(
+            day = 20,
             name = R.string.ReverseLunges,
             imgUrl = R.drawable.day_20_reverse_lunges,
             description = R.string.ReverseLunges_description
         ),
         Exercise(
+            day = 21,
             name = R.string.ButtKicks,
             imgUrl = R.drawable.day_21_butt_kicks,
             description = R.string.ButtKicks_description
         ),
         Exercise(
+            day = 22,
             name = R.string.Climbers,
             imgUrl = R.drawable.day_22_climbers,
             description = R.string.Climbers_description
         ),
         Exercise(
+            day = 23,
             name = R.string.Inchworm,
             imgUrl = R.drawable.day_23_inchworm,
             description = R.string.Inchworm_description
         ),
         Exercise(
+            day = 24,
             name = R.string.HipThrust,
             imgUrl = R.drawable.day_24_hip_thrust,
             description = R.string.HipThrust_description
         ),
         Exercise(
+            day = 25,
             name = R.string.BoxJumps,
             imgUrl = R.drawable.day_25_box_jumps,
             description = R.string.BoxJumps_description
         ),
         Exercise(
+            day = 26,
             name = R.string.StepUps,
             imgUrl = R.drawable.day_26_step_ups,
             description = R.string.StepUps_description
         ),
         Exercise(
+            day = 27,
             name = R.string.DonkeyKicks,
             imgUrl = R.drawable.day_27_donkey_kicks,
             description = R.string.DonkeyKicks_description
         ),
         Exercise(
+            day = 28,
             name = R.string.Windmills,
             imgUrl = R.drawable.day_28_wind_mills,
             description = R.string.Windmills_description
         ),
         Exercise(
+            day = 29,
             name = R.string.BearCrawl,
             imgUrl = R.drawable.day_29_bear_crawl,
             description = R.string.BearCrawl_description
         ),
         Exercise(
+            day = 30,
             name = R.string.VUps,
             imgUrl = R.drawable.day_30_v_ups,
             description = R.string.VUps_description
         )
+
 
     )
 }
