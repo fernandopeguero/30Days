3ca70b74ce419b7d913eab6636763bce5553e8af
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-11 22:38:58 -0500

    Add compose functions to display exercise list

--- app/src/main/java/com/example/a30days/MainActivity.kt
+++ app/src/main/java/com/example/a30days/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.a30days.screens.ExerciseApp
 import com.example.a30days.ui.theme._30DaysTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             _30DaysTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   ExerciseApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     _30DaysTheme {
-        Greeting("Android")
+       ExerciseApp()
     }
 }
---
+++ app/src/main/java/com/example/a30days/screens/ExercisesScreen.kt
@@ -0,0 +1,97 @@
+package com.example.a30days.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a30days.R
+import com.example.a30days.model.DataRepository
+import com.example.a30days.model.Exercise
+import com.example.a30days.ui.theme._30DaysTheme
+
+
+@Composable
+fun ExerciseCard(exercise:Exercise, modifier: Modifier = Modifier) {
+
+    Card (
+        modifier = modifier
+    ){
+        Column {
+            Row {
+                Text(text = stringResource(R.string.day, exercise.day))
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = null
+                )
+            }
+            Text(
+                text = stringResource(exercise.name)
+            )
+
+            Image(
+                painter = painterResource(exercise.imgUrl),
+                contentDescription = stringResource( exercise.name),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.sizeIn(maxHeight = 250.dp).fillMaxWidth()
+            )
+
+            Text(
+                text = stringResource(exercise.description)
+            )
+        }
+    }
+
+}
+
+@Composable
+fun ExerciseList(exercises: List<Exercise>, modifier: Modifier = Modifier) {
+
+
+    LazyColumn {
+
+        items(exercises) {
+            ExerciseCard(
+                it,
+                modifier = Modifier
+            )
+        }
+    }
+
+}
+
+@Composable
+fun ExerciseApp(modifier: Modifier = Modifier) {
+
+    val list = DataRepository.exercises
+
+    ExerciseList(list, modifier = modifier)
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewExercise() {
+
+    val day = DataRepository.exercises
+    _30DaysTheme {
+        ExerciseApp()
+    }
+}
