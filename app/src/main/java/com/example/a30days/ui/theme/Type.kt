ec83beefa15b910939220ed1906d2fbfa804c98b
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-13 16:40:32 -0500

    Add typography types Cabin, Montserrat to project theme

--- app/src/main/java/com/example/a30days/ui/theme/Type.kt
+++ app/src/main/java/com/example/a30days/ui/theme/Type.kt
@@ -2,9 +2,20 @@ package com.example.a30days.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.a30days.R
+
+val Cabin = FontFamily(
+    Font(R.font.cabin_regular)
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,6 +25,23 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily =  Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+    ),
+
+    titleSmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
