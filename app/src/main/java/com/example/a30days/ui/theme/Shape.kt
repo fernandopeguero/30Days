9c401de9504999a66f30b7f25bbd4d2132ae3b82
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-13 16:38:10 -0500

    Update to use the size in dimen resources

--- app/src/main/java/com/example/a30days/ui/theme/Shape.kt
+++ app/src/main/java/com/example/a30days/ui/theme/Shape.kt
@@ -2,10 +2,12 @@ package com.example.a30days.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
+import com.example.a30days.R
 
 
 val Shapes  = Shapes(
-    small = RoundedCornerShape(8.dp),
-    medium = RoundedCornerShape(16.dp),
+    small = RoundedCornerShape(R.dimen.padding_small),
+    medium = RoundedCornerShape( R.dimen.padding_small),
 )
